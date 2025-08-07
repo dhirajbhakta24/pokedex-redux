ad3a9ec561646e8a0a48d389849b8a39621514e3
Dhiraj Kumar Bhakta <bhaktadhiraj@gmail.com>
2025-08-08 01:24:25 +0530

    setup basic pokedex app

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,13 @@
+function Search() {
+  return (
+    <div className="search-wrapper">
+      <input
+        id="pokemon-name-search"
+        placeholder="pokemon name ..."
+        type="text"
+      />
+    </div>
+  );
+}
+
+export default Search;
