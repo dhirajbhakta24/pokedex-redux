740bc1529a94922135cffa81c5acc25afe66bd27
Dhiraj Kumar Bhakta <bhaktadhiraj@gmail.com>
2025-08-10 17:16:20 +0530

    added routing and pokemon-details component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import "./App.css";
 import Pokedex from "./components/Pokedex/Pokedex";
+import CustomRoutes from "./Routes/CustomRoutes";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Pokedex />
+      <div className="outer-pokedex">
+        <h1 id="pokedex-heading">
+          {/* //clicking on Pokedex will let us get to
+          url "/" */}
+          <Link to="/">Pokedex</Link>
+        </h1>
+        <CustomRoutes />
+      </div>
     </>
   );
 }
